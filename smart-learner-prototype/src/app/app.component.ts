7a43ca0adbcab8562d814b71200e598983f27735
abir-abdelli <abyr.abdelli@gmail.com>
2023-09-22 05:43:17 +0100

    first version of chatbot

--- smart-learner-prototype/src/app/app.component.ts
+++ smart-learner-prototype/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import OpenAI from 'openai';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smart-learner-prototype';
+  userMessage!: string;
+  assistantReply!: string;
+  chatMessages: { role: string, content: string }[] = [];
+  
+  
+
+  public async sendMessage() {
+    const openai = new OpenAI({
+      apiKey: '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',
+      dangerouslyAllowBrowser: true
+    });
+      const userMessage = this.userMessage;
+      this.chatMessages.push({role: 'user', content: userMessage});
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+      });
+    
+      const reply = chatCompletion.choices[0].toString();
+      this.chatMessages.push({role: 'assistant', content: reply})
+    
+  }
 }
